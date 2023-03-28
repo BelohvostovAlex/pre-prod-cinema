82b4a1be52f15576919858b9900b926d807f0f6f
Alex <belohvostov.belohvostov@gmail.com>
2023-03-28 12:09:36 +0300

    added google fonts by link, deleted not actual test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
-import App from "./App";
-
-test("renders learn react link", () => {
-  render(<App />);
-  const text = screen.getByText(/app/i);
-  expect(text).toBeInTheDocument();
+describe("App", () => {
+  it("renders learn react link", () => {});
 });
